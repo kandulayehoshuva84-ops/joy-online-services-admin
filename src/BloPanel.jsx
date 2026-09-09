45fc65a2c63ebc6626a555c8f2c2f7c404d6f4a6
kandulayehoshuva84-ops <kandulayehoshuva84@gmail.com>
2026-09-09 16:47:12 +0530

    Update BloPanel.jsx

--- src/BloPanel.jsx
+++ src/BloPanel.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react';
 import { supabase } from './supabase';
 import BloMobileLink from './BloMobileLink';
+import TemplateProcessor from './TemplateProcessor';
 
 const BUCKET = 'Documents';
 const EMPTY_VOTER = {
